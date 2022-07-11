b231c8b72bc0cde3c50f1f6c2778f719e9e71af9
Denyce Edwards <denyceedwards@Denyces-MacBook-Pro.local>
2022-07-11 18:59:50 +0100

    first commit

---
+++ src/test/java/com/qa/spring_nftmetadata_app/SpringNftMetadataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.spring_nftmetadata_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNftMetadataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
